eb910b1816166620c21d007c9b432ac10d3361e6
lilyanapet <45791543+lilyanapet@users.noreply.github.com>
2019-04-04 17:27:50 +0300

    Create XlsFileUploadSection.cs

---
+++ Application/XlsFileUploadSection.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using umbraco.businesslogic;
+using umbraco.interfaces;
+
+namespace App_Plugins.XlsFileUpload.Application
+{
+    [Application("xlsFileUpload", "XlsFileUpload", "icon-cloud-upload", 13)]
+    public class XlsFileUploadSection : IApplication
+    {
+    }
+}
